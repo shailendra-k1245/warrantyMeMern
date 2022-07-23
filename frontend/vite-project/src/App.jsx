78beeffeb89462229ca35f0cc9ff8e2f38704efc
Shailendra <91621557+shailendra-k1245@users.noreply.github.com>
2022-07-23 09:03:21 +0530

    msg

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -5,20 +5,44 @@ import { Route, Routes, Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+import { MdDashboard } from "react-icons/md";
+import { GrTableAdd } from "react-icons/gr";
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <div className="a">
+        <Navbar bg="primary" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </div>
+      <div className="b">
+        <Card style={{ width: "18rem" }}>
+          <Card.Header>Featured</Card.Header>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              {" "}
+              <MdDashboard /> Dashboard
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <GrTableAdd /> Tables
+            </ListGroup.Item>
+            <ListGroup.Item>Billing</ListGroup.Item>
+            <ListGroup.Item>Virtual Reality</ListGroup.Item>
+            <ListGroup.Item>RTL</ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </div>
     </div>
   );
 }
